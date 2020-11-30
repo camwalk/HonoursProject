22b936aeb8585ad8eb2b7f7529dd6a34c5821158
camwalk <55843608+camwalk@users.noreply.github.com>
2020-11-30 20:38:16 +0000

    added repository with automapper & extended user

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -1,18 +1,16 @@
 using System;
 using System.Collections.Generic;
-using API.Extensions;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class AppUser
+    public class MemberDto
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public string Gender { get; set; }
         public string Introduction { get; set; }
@@ -20,6 +18,7 @@ namespace API.Entities
         public string Background { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+        public ICollection<PhotoDto> Photos { get; set; }
+
     }
 }
