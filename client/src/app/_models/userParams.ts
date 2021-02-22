cb755f6fa1aa2cc5312876d02e2be721157dfbbb
camwalk <55843608+camwalk@users.noreply.github.com>
2021-02-22 11:02:08 +0000

    attempt at adding filters

--- client/src/app/_models/userParams.ts
+++ client/src/app/_models/userParams.ts
@@ -1,5 +1,14 @@
+import { User } from "./user";
+
 export class UserParams {
     gender: string;
     pageNumber: 1;
     pageSize: 5;
+    searchLocation = 'Reunion';
+    searchInstrument = 'Guitar';
+    sortBy = 'lastActive';
+
+    constructor(UserParams: User){
+            
+    }
 }
