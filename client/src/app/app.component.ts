0627888b5dfba56d4c8b959dd277272cbfb3854f
camwalk <55843608+camwalk@users.noreply.github.com>
2021-03-01 12:48:13 +0000

    before push

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AccountService } from './_services/account.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Honours Project';
+  title = 'Client';
   users: any;
 
   constructor(private accountService: AccountService) { }
